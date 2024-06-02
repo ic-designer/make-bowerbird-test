3cd374f97edb0eab89d803f335626f755406e5b5
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-01 23:22:30 -0500

    added dependencies to the repo

--- make/deps.mk
+++ make/deps.mk
@@ -1,2 +1,15 @@
 # Constants
 WORKDIR_DEPS ?= $(error ERROR: Undefined variable WORKDIR_DEPS)
+
+# Load Bowerbird Dependency Tools
+BOWERBIRD_DEPS.MK := $(WORKDIR_DEPS)/BOWERBIRD_DEPS/bowerbird_deps.mk
+$(BOWERBIRD_DEPS.MK):
+	@curl --silent --show-error --fail --create-dirs -o $@ -L \
+https://raw.githubusercontent.com/ic-designer/make-bowerbird-deps/\
+a84d91d5d726ab8639b330b453a3d899556b480f/src/bowerbird-deps/bowerbird-deps.mk
+include $(BOWERBIRD_DEPS.MK)
+
+
+# Load Dependencies
+$(eval $(call bowerbird::git-dependency,BOWERBIRD_HELP,https://github.com/ic-designer/make-bowerbird-help.git,main,bowerbird.mk))
+$(eval $(call bowerbird::git-dependency,BOWERBIRD_GIT,https://github.com/ic-designer/make-bowerbird-git.git,main,bowerbird.mk))
