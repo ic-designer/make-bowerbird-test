c86d6bd11de336e4e0222153034e36f58ff3a48b
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-05 21:18:17 -0500

    improve the tests for the decorator function

--- test/bowerbird-test/test-find-test-files.mk
+++ test/bowerbird-test/test-find-test-files.mk
@@ -3,7 +3,7 @@ $(call bowerbird::test::find-test-files,test/mock-tests,mock-test*.mk)
 endef
 
 test-find-test-files-mock-path-num-files:
-	$(call bowerbird::test::compare-string,3,$(words $(bowerbird::test::mock-test-files)))
+	$(call bowerbird::test::compare-strings,3,$(words $(bowerbird::test::mock-test-files)))
 
 test-find-test-files-mock-path-alpha:
 	$(call bowerbird::test::compare-sets,$(filter %alpha.mk,$(bowerbird::test::mock-test-files)),$(abspath test/mock-tests/alpha/mock-test-alpha.mk))
--- test/bowerbird-test/test-run-test-decorator.mk
+++ test/bowerbird-test/test-run-test-decorator.mk
@@ -1,28 +1,51 @@
-test-run-test-decorator-failing-test:
-	! $(MAKE) @bowerbird-test/run-test/bowerbird-test/failing-test
-	test -f $(WORKDIR_TEST)/bowerbird-test/failing-test/failing-test.$(BOWERBIRD_TEST_STDOUT_EXT)
-	test -f $(WORKDIR_TEST)/bowerbird-test/failing-test/failing-test.$(BOWERBIRD_TEST_STDERR_EXT)
+test-run-test-decorator-failing-test-log-file-names:
+	! $(MAKE) @bowerbird-test/run-test/mock-test/$@/failing-test
+	test -f $(WORKDIR_TEST)/mock-test/$@/failing-test/failing-test.$(BOWERBIRD_TEST_STDOUT_EXT)
+	test -f $(WORKDIR_TEST)/mock-test/$@/failing-test/failing-test.$(BOWERBIRD_TEST_STDERR_EXT)
 
-.PHONY: bowerbird-test/failing-test
-bowerbird-test/failing-test:
+test-run-test-decorator-failing-test-printed-response:
+	$(call bowerbird::test::compare-strings,\
+			$(shell $(MAKE) @bowerbird-test/run-test/mock-test/$@/failing-test 2>/dev/null),\
+			$(shell printf "\e[1;31mFailed\e[0m: mock-test/test-run-test-decorator-failing-test-printed-response/failing-test"))
+
+test-run-test-decorator-passing-test-log-file-names:
+	$(MAKE) @bowerbird-test/run-test/mock-test/$@/passing-test
+	test -f $(WORKDIR_TEST)/mock-test/$@/passing-test/passing-test.$(BOWERBIRD_TEST_STDOUT_EXT)
+	test -f $(WORKDIR_TEST)/mock-test/$@/passing-test/passing-test.$(BOWERBIRD_TEST_STDERR_EXT)
+
+test-run-test-decorator-passing-test-printed-response:
+	$(call bowerbird::test::compare-strings,\
+			$(shell $(MAKE) @bowerbird-test/run-test/mock-test/$@/passing-test 2>/dev/null),\
+			$(shell printf "\e[1;32mPassed\e[0m: mock-test/test-run-test-decorator-passing-test-printed-response/passing-test"))
+
+test-run-test-decorator-hierarchical-name-log-file-names:
+	$(MAKE) @bowerbird-test/run-test/mock-test/$@/alpha/beta/passing-test
+	test -f $(WORKDIR_TEST)/mock-test/$@/alpha/beta/passing-test/passing-test.$(BOWERBIRD_TEST_STDOUT_EXT)
+	test -f $(WORKDIR_TEST)/mock-test/$@/alpha/beta/passing-test/passing-test.$(BOWERBIRD_TEST_STDERR_EXT)
+
+test-run-test-decorator-undefined-variable-check-is-undefined:
+	! $(MAKE) mock-test/force BOWERBIRD_TEST_VARIABLE=true
+
+test-run-test-decorator-undefined-variable-printed-response:
+	$(call bowerbird::test::compare-strings,\
+			$(shell $(MAKE) @bowerbird-test/run-test/mock-test/$@/undefined-variable-test 2>/dev/null),\
+			$(shell printf "\e[1;31mFailed\e[0m: mock-test/test-run-test-decorator-undefined-variable-printed-response/undefined-variable-test"))
+
+
+mock-test/%/passing-test: mock-test/force
+	exit 0
+
+mock-test/%/failing-test: mock-test/force
 	exit 1
 
+mock-test/%/undefined-variable-test: mock-test/force
+	echo $(BOWERBIRD_TEST_VARIABLE)
 
-test-run-test-decorator-passing-test:
-	$(MAKE) @bowerbird-test/run-test/bowerbird-test/passing-test
-	test -f $(WORKDIR_TEST)/bowerbird-test/passing-test/passing-test.$(BOWERBIRD_TEST_STDOUT_EXT)
-	test -f $(WORKDIR_TEST)/bowerbird-test/passing-test/passing-test.$(BOWERBIRD_TEST_STDERR_EXT)
-
-.PHONY: bowerbird-test/passing-test
-bowerbird-test/passing-test:
-	exit 0
+ifdef BOWERBIRD_TEST_VARIABLE
+    $(error ERROR: Unable to run test with: BOWERBIRD_TEST_VARIABLE is not an undefined variable.)
+endif
 
 
-test-run-test-decorator-hierarchical-name:
-	$(MAKE) @bowerbird-test/run-test/bowerbird-test/alpha/beta/gamma/hierarchical-name
-	test -f $(WORKDIR_TEST)/bowerbird-test/alpha/beta/gamma/hierarchical-name/hierarchical-name.$(BOWERBIRD_TEST_STDOUT_EXT)
-	test -f $(WORKDIR_TEST)/bowerbird-test/alpha/beta/gamma/hierarchical-name/hierarchical-name.$(BOWERBIRD_TEST_STDERR_EXT)
-
-.PHONY: bowerbird-test/alpha/beta/gamma/hierarchical-name
-bowerbird-test/alpha/beta/gamma/hierarchical-name:
-	exit 0
+.PHONY: mock-test/force
+.mock-test/force:
+	@:
