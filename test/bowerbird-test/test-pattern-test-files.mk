524488466ddb516ae7392595e833ed786a377e76
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-12 22:04:50 -0500

    created tests for test file discovery patterns

---
+++ test/bowerbird-test/test-pattern-test-files.mk
@@ -0,0 +1,21 @@
+$(call bowerbird::test::pattern-test-files,*alpha.mk)
+$(call bowerbird::generate-test-runner,test-pattern-test-files-alpha-runner,test/mock-tests)
+test-pattern-test-files-alpha:
+	$(call bowerbird::test::compare-sets,$(BOWERBIRD_TEST_FILES/$@-runner),\
+			$(abspath test/mock-tests/alpha/mock-test-alpha.mk))
+
+
+$(call bowerbird::test::pattern-test-files,*beta.mk)
+$(call bowerbird::generate-test-runner,test-pattern-test-files-beta-runner,test/mock-tests)
+test-pattern-test-files-beta:
+	$(call bowerbird::test::compare-sets,\
+			$(BOWERBIRD_TEST_FILES/$@-runner),\
+			$(abspath test/mock-tests/alpha/beta/mock-test-beta.mk))
+
+
+$(call bowerbird::test::pattern-test-files,*gamma.mk)
+$(call bowerbird::generate-test-runner,test-pattern-test-files-gamma-runner,test/mock-tests)
+test-pattern-test-files-gamma:
+	$(call bowerbird::test::compare-sets,\
+			$(BOWERBIRD_TEST_FILES/$@-runner),\
+			$(abspath test/mock-tests/alpha/beta/gamma/mock-test-gamma.mk))
