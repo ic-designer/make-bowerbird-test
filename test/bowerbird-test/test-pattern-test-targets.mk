a3573bf189f0c101d08123f8b2b580babe8c2630
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-12 22:05:09 -0500

    created tests for test target discovery patterns

---
+++ test/bowerbird-test/test-pattern-test-targets.mk
@@ -0,0 +1,25 @@
+$(call bowerbird::test::pattern-test-files,mock*.mk)
+
+
+$(call bowerbird::test::pattern-test-targets,test*alpha*)
+$(call bowerbird::generate-test-runner,test-pattern-test-targets-alpha-runner,test/mock-tests)
+test-pattern-test-targets-alpha:
+	$(call bowerbird::test::compare-sets,\
+			$(sort $(BOWERBIRD_TEST_TARGETS/$@-runner)),\
+			$(sort test-find-files-alpha-1 test-find-files-alpha-2))
+
+
+$(call bowerbird::test::pattern-test-targets,test*beta*)
+$(call bowerbird::generate-test-runner,test-pattern-test-targets-beta-runner,test/mock-tests)
+test-pattern-test-targets-beta:
+	$(call bowerbird::test::compare-sets,\
+			$(sort $(BOWERBIRD_TEST_TARGETS/$@-runner)),\
+			$(sort test-find-files-beta-1 test-find-files-beta-2))
+
+
+$(call bowerbird::test::pattern-test-targets,test*gamma*)
+$(call bowerbird::generate-test-runner,test-pattern-test-targets-gamma-runner,test/mock-tests)
+test-pattern-test-targets-gamma:
+	$(call bowerbird::test::compare-sets,\
+			$(sort $(BOWERBIRD_TEST_TARGETS/$@-runner)),\
+			$(sort test-find-files-gamma-1 test-find-files-gamma-2))
