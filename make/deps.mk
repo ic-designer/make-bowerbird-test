b03a3354cc7ae0db2cf565ba23b7e721444f582b
Jeff Fredenburg <136350727+jfredenburg@users.noreply.github.com>
2026-01-20 18:33:02 -0600

    Remove confusing bowerbird-core.entry variable
    The entry parameter conflicts with git-dependency's entry parameter
    which refers to the final entry point file. The loader file is always
    'bowerbird-loader.mk' and doesn't need to be configurable.
    
    This matches the fix in make-bowerbird-core.

--- make/deps.mk
+++ make/deps.mk
@@ -2,15 +2,15 @@
 WORKDIR_DEPS ?= $(error ERROR: Undefined variable WORKDIR_DEPS)
 
 # Bootstrap bowerbird-core
-bowerbird-core.path ?= $(WORKDIR_DEPS)/bowerbird-core
 bowerbird-core.url ?= https://github.com/asikros/make-bowerbird-core.git
 bowerbird-core.branch ?= main
-bowerbird-core.entry ?= bowerbird.mk
-$(WORKDIR_DEPS)/bowerbird-loader.mk:
+bowerbird-core.path ?= $(WORKDIR_DEPS)/bowerbird-core
+
+$(bowerbird-core.path)/bowerbird-loader.mk:
 	@curl --silent --show-error --fail --create-dirs -o $@ -L \
 https://raw.githubusercontent.com/asikros/make-bowerbird-core/$(bowerbird-core.branch)/bowerbird-loader.mk
 
-include $(WORKDIR_DEPS)/bowerbird-loader.mk
+include $(bowerbird-core.path)/bowerbird-loader.mk
 
 # Load Dependencies using kwargs-based API
 $(call bowerbird::core::git-dependency, \
